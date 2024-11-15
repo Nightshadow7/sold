f320fcec0b3dd3284b4feede49da68903635d2b6
Nightshadow7 <hernandezdiazanthonilexandre@gmail.com>
2024-11-14 20:18:25 -0500

    feat :boom: Refactor absolute code Se cambio totalmente el codigo

---
+++ Clase14-11-2024/area del circulo.py
@@ -0,0 +1,25 @@
+import math as mt
+
+# definicion de funciones 
+def saludar(nombre):
+  print(f"Hola, {nombre}!")
+  
+def calcular_area_circulo(radio):
+  return mt.pi * radio ** 2
+
+# funcion principal
+def main():
+  # variables y entrada de datos
+  nombre_usuario = input("¿Como te llamas?")
+  saludar(nombre_usuario)
+
+  try:
+    radio = float(input("Ingresa el radio de un circulo: "))
+    area = calcular_area_circulo(radio)
+    print(f"El radio del circulo es: {area:.2f}")
+  except ValueError:
+    print("Por favor, ingresa un numero valido para el radio.")
+
+# ejecutar el programa
+if __name__ == "__main__":
+  main()
